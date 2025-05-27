6dec251025d73e2ba6f19e22bb380492c3b34475
Abhishek Dangi <adangi555@gmail.com>
2025-05-27 03:19:32 +0000

    Why did the app become so slow, get rid of un-necessary code

--- src/app/dealer-ledger/page.tsx
+++ src/app/dealer-ledger/page.tsx
@@ -1,4 +1,3 @@
-
 // This file has been replaced by /src/app/parties/page.tsx
 // It can be safely deleted.
 // If you see this message, it means the old file was not removed by the process.
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // This file is being removed as Firestore integration is temporarily disabled.
 // If you re-enable Firestore, you will need to restore this file with your
 // Firebase project configuration.
